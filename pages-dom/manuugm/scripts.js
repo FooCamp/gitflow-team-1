097b280262631bdb5e7a716a8e2ddfcbafce0e1e
Manuela Gonzalez <manuu-gonzalez@hotmail.com>
2020-07-06 17:40:12 -0500

    Create dom page exercise

---
+++ pages-dom/manuugm/scripts.js
@@ -0,0 +1,25 @@
+const myUser = {
+    displayName: "Manuela González",
+    userName: "",
+    image: "",
+    role: "developer",
+    socialMedia: [
+      {
+        name: "Github",
+        icon: "",
+        link: ""
+      },
+      {
+        name: "Twitter",
+        icon: "",
+        link: ""
+      },
+      {
+        name: "Instagram",
+        icon: "",
+        link: ""
+      },
+    ],
+  }
+
+//Funtion for my elements and class
